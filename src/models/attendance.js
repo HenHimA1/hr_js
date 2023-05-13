d13d2b56a1109e117093710d1573521e2f0e4144
HenHimA1 <hendrahima1@gmail.com>
2023-05-13 21:32:08 +0700

    update position

--- src/models/attendance.js
+++ src/models/attendance.js
@@ -1,6 +1,7 @@
 import moment from "moment";
 import { Schema, model } from "mongoose";
 import { User } from "./user";
+import { Company } from "./company";
 
 const AttendanceSchema = Schema({
   check_in: { required: true, type: String },
@@ -8,18 +9,19 @@ const AttendanceSchema = Schema({
   user_id: { required: true, type: Schema.Types.ObjectId, ref: "user" },
   create_date: { type: String },
   is_valid: { default: false, type: Boolean },
-  latitude: { type: Number, require: true },
-  longitude: { type: Number, require: true },
+  latitude: { type: Number, required: true },
+  longitude: { type: Number, required: true },
 });
 
 const getCurrentTime = () => moment().format("HH:mm:ss DD-MM-YYYY");
 
 AttendanceSchema.pre("validate", async function (next) {
-  let isUserExist = await User.exists({ _id: this.user_id });
-  if (!isUserExist) next(new Error("User not found"));
+  const currentUser = await User.findOne({ _id: this.user_id });
+  if (!currentUser) next(new Error("User not found"));
+  const currentCompany = await Company.findOne({ _id: currentUser.company_id });
+  if (!currentCompany.latitude || !currentCompany.longitude)
+    next(new Error("Company position not defined"));
   if (this.isNew) this.create_date = getCurrentTime();
-  if (!this.latitude || !this.longitude)
-    next(new Error("Position not defined"));
   next();
 });
 
