ea048c31b782078bfc8a2a108c3f16ac84a0e698
juke-hendra <hendra.himawan@jukesolutions.com>
2023-05-28 22:30:12 +0700

    [update]index

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,10 @@
-import { Router } from "express";
+import express, { Router } from "express";
 import { ApiRouter } from "./api";
 import { join } from "path";
 
 const AppRouter = Router();
 
-AppRouter.get("/", function (req, res) {
-  res.sendFile(join(__dirname, '../../public/index.html'))
-});
-
+AppRouter.use("/", express.static(join(__dirname, '../../public')))
 AppRouter.use(ApiRouter);
 
 export { AppRouter };
