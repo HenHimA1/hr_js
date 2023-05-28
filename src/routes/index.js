75f1d7692b598e6b3a19ae52d0cda744a387286a
juke-hendra <hendra.himawan@jukesolutions.com>
2023-05-28 12:17:33 +0700

    [add]public html

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { ApiRouter } from "./api";
+import { join } from "path";
 
 const AppRouter = Router();
 
 AppRouter.get("/", function (req, res) {
-  res.redirect("/docs");
+  res.sendFile(join(__dirname, '../../public/index.html'))
 });
 
 AppRouter.use(ApiRouter);
