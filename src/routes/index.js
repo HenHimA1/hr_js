e048a70ca8dc005431a3473f1682e00b50cebb62
juke-hendra <hendra.himawan@jukesolutions.com>
2023-06-10 18:42:04 +0700

    [update] static

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ import { join } from "path";
 
 const AppRouter = Router();
 
-AppRouter.use("/", express.static(join(__dirname, '../../public')))
+AppRouter.use("/", express.static(join(__dirname, '../../public/dist')))
 AppRouter.use(ApiRouter);
 AppRouter.use((req, res, next) => {
     res.status(400)
